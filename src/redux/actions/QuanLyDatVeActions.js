3a6aa3f34f8433cb3e707f9ce9e077cc788cd31f
lamcuong <lamcuong1611@gmail.com>
2022-12-12 08:57:50 +0700

    hoan thien du an

--- src/redux/actions/QuanLyDatVeActions.js
+++ src/redux/actions/QuanLyDatVeActions.js
@@ -3,18 +3,20 @@ import { HIDE_LOADING, OPEN_LOADING } from "./types/LoadingTypes"
 import { CHUYEN_TAB, DAT_VE_HOAN_TAT, LAY_DANH_SACH_PHONG_VE } from "./types/QuanLyDatVeTypes"
 
 export const layDanhSachPhongVe = (maLichChieu) => {
+
     return async (dispatch) => {
+        dispatch({ type: OPEN_LOADING })
         try {
             const result = await quanLyDatVeServices.layDanhSachPhongVe(maLichChieu)
 
-
+            dispatch({ type: HIDE_LOADING })
             dispatch({
                 type: LAY_DANH_SACH_PHONG_VE,
                 chiTietPhongVe: result.data.content
             })
 
         } catch (err) {
-
+            dispatch({ type: HIDE_LOADING })
             console.log(err)
         }
 
@@ -23,8 +25,9 @@ export const layDanhSachPhongVe = (maLichChieu) => {
 export const actionDatVe = (thongTinDatVe) => {
 
     return async (dispatch) => {
+        dispatch({ type: OPEN_LOADING })
         try {
-            dispatch({ type: OPEN_LOADING })
+
             const result = await quanLyDatVeServices.actionDatVe(thongTinDatVe)
 
             await dispatch(layDanhSachPhongVe(thongTinDatVe.maLichChieu))
